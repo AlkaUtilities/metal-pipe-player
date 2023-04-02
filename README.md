858b9e00f91331fd23f809d3fa1e626e75c160de
aldenizenmc <63831735+AldenizenMC@users.noreply.github.com>
2023-04-02 13:12:17 +0700

    Added (2) - Added the ability to add custom audio and switch between audio(s) using a command - Added ffmpeg-static

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # Metal Pipe Player
 goofy ass bot that has no reason other than to annoy people.
-
 infinite amounts of silence occasionally broken by metal pipe falling
 
-## info
-help.mp3: 1 hour of silence occasinally broken by metal pipe falling looped. [original video](https://www.youtube.com/watch?v=gFrmJrTVtO8)
+## Installation
+1. Install [Node.js](https://nodejs.org/en)
+2. Copy GitHub repository
+3. Create a discord application. [(click here)](https://discord.com/developers/applications "click here")
+4. Open the application and go to **Bot** (on the left sidebar) and click **Add Bot**
+5. Click **Reset Token** and enter 2FA code if you have it enabled and copy token
+6. Scroll down to **Privileged Gateway Intents** and enable **Message Content Intent**
+7. Go to **OAuth2** and **URL Generator**, on **Scopes** select **bot** and select **Connect** and **Speak** (its on the Voice Permissions section) and invite the bot using the generated url
+8. Create a `.env` file and type `TOKEN=REPLACE_THIS_WITH_YOUR_TOKEN` so it should look something like `TOKEN=MTA2NjYyNTg0MzUyNDI3MjIxMg.GkLoPp.iA0UM1JXHp4FgK7952C_L9JNXrpXhMMyoL4Hfc`
+9. On the terminal, run `npm install` and `node .`
 
-short.mp3: metal pipe sfx (very violent (very loud)). [original video](https://www.youtube.com/watch?v=FtyZLLdY19Y)
+## Usage
+
+#### Inviting the bot to a voice channel
+Join a voice channel and type `-play`
+
+#### Changing audio
+Type `-mode` to see a list of available audio(s) to play.
+Type `-mode <index>` to switch to the selected audio
+
+#### Adding your own audio
+1. Add your audio to the audio folder
+2. Open `audio.json`
+3. Create a new object
+4. Fill the **path** with `./audio/FILE_NAME.mp3`
+5. Fill the **name** and **src** with whatever you want
+6. Your audio should be visible in the library
+
+#### Disconnecting the bot from a voice channel
+Get an admin to manually kick the bot from the voice channel
--- index.js
+++ index.js
@@ -5,61 +5,146 @@
 // alden: it was ur idea
 // kyhri: stfu
 
-require('dotenv').config();
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-const { Client, Intents } = require('discord.js');
-const { spawn } = require('child_process');
+require("dotenv").config();
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    AudioPlayerStatus,
+} = require("@discordjs/voice");
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const { spawn } = require("child_process");
+const config = require("./audio.json");
 
-// this uses ALL intent which is bad practice, 
-// i was lazy to figure out which intent it needed tho 
+let currentAudio = {
+    name: config[0].name ?? config[0].path,
+    path: config[0].path,
+};
+
+// this uses ALL intent which is bad practice,
+// i was lazy to figure out which intent it needed tho
 // - aldenizenmc
 const client = new Client({ intents: 131071 });
 
-const prefix = '-'; // prefix to run command (change it to whatever you like)
+const prefix = "-"; // prefix to run command (change it to whatever you like)
 
-client.on('ready', () => {
+client.on("ready", () => {
     client.user.setPresence({
-        activities: [{
-            name: "-play",
-            type: "STREAMING",
-            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        }]
+        activities: [
+            {
+                name: "-play",
+                type: "STREAMING",
+                url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            },
+        ],
     });
 
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(' ');
+    const args = message.content.slice(prefix.length).trim().split(" ");
     const command = args.shift().toLowerCase();
 
     switch (command) {
-        case "play": {
-            if (!message.member.voice.channel) {
-                return message.reply('You need to join a voice channel first!');
+        case "play":
+            {
+                if (!message.member.voice.channel) {
+                    return message.reply(
+                        "You need to join a voice channel first!"
+                    );
+                }
+
+                if (currentAudio.path == undefined) {
+                    return message.reply(
+                        "Audio path is undefined.\nPlease reset using `mode <index>`"
+                    );
+                }
+
+                const connection = joinVoiceChannel({
+                    channelId: message.member.voice.channel.id,
+                    guildId: message.guild.id,
+                    adapterCreator: message.guild.voiceAdapterCreator,
+                });
+
+                // i have no idea what any of this does but if it works, it works
+                const player = createAudioPlayer();
+                player.play(createAudioResource(currentAudio.path));
+                connection.subscribe(player);
+                player.on(AudioPlayerStatus.Idle, () => {
+                    player.play(createAudioResource(currentAudio.path));
+                });
+
+                const guild = client.guilds.cache.get(message.guild.id);
+                const channel = guild.channels.cache.get(
+                    message.member.voice.channel.id
+                );
+
+                message.reply(
+                    `Playing \` ${currentAudio.name} \` at \` #${channel.name} \` on loop`
+                );
             }
-    
-            const audioFile = "./help.mp3"; //.mp3 file path
-            
-            const connection = joinVoiceChannel({
-                channelId: message.member.voice.channel.id,
-                guildId: message.guild.id,
-                adapterCreator: message.guild.voiceAdapterCreator,
-            });
-    
-            // i have no idea what any of this does but if it works, it works
-            const player = createAudioPlayer();
-            player.play(createAudioResource(audioFile));
-            connection.subscribe(player);
-            player.on(AudioPlayerStatus.Idle, () => {
-                player.play(createAudioResource(audioFile));
-            })
-        };
-        break;
+            break;
+
+        case "mode":
+            {
+                if (!args.length) {
+                    const embed = new MessageEmbed()
+                        .setTitle("Library")
+                        .setDescription(
+                            `Usage: \`${prefix}mode <index>\`\n\n` +
+                                config
+                                    .map(
+                                        (i) =>
+                                            `\` ${config.indexOf(i)} \` ${
+                                                i.name
+                                            }`
+                                    )
+                                    .join("\n")
+                        );
+                    return message.reply({ embeds: [embed] });
+                }
+
+                if (isNaN(args[0]))
+                    return message.reply(
+                        `Argument is not a number.\nInput \` ${args[0]} \` `
+                    );
+
+                if (config[args[0]] === undefined) {
+                    const embed = new MessageEmbed()
+                        .setTitle("Library")
+                        .setDescription(
+                            `Usage: \`${prefix}mode <index>\`\n` +
+                                `Error: Index \` ${args[0]} \` is undefined.\n\n` +
+                                config
+                                    .map(
+                                        (i) =>
+                                            `\` ${config.indexOf(i)} \` ${
+                                                i.name
+                                            }`
+                                    )
+                                    .join("\n")
+                        );
+                    return message.reply({ embeds: [embed] });
+                }
+
+                const prevAudio = currentAudio;
+
+                currentAudio = {
+                    name: config[args[0]].name ?? config[args[0]].path,
+                    path: config[args[0]].path,
+                };
+
+                message.reply(
+                    `Switched from \` ${
+                        prevAudio.name ?? prevAudio.path
+                    } \` to \` ${currentAudio.name ?? currentAudio.path} \``
+                );
+            }
+            break;
     }
-    
 });
 
 client.login(process.env.TOKEN);
