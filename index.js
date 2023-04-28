1e8c5edbd46fef7e6e96f1067b01b7c9e368561f
AldenizenMC <63831735+AldenizenMC@users.noreply.github.com>
2023-04-28 22:02:21 +0700

    FIxed express server

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ client.subCommands = new Collection();
 load_events(client);
 
 // uncomment the line below for monitoring purposes
-// require("express")().listen(3000, () => console.log("Listening on port 3000"));
+// require("express")().get("/", (_, res) => res.sendStatus(200)).listen(3000, () => console.log("Listening on port 3000"));
 
 client.login(process.env.TOKEN);
